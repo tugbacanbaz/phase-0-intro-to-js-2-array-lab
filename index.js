548558ffa2ed310f455b53ee59cf0ba11d67ea14
tugba <tugbacanbaz.pi@gmail.com>
2022-10-15 16:42:27 +0300

    Complited Assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+let cats=["Milo","Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    cats.push("Ralph")
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+
+function appendCat (name) {
+    return [...cats, "Broom"];
+  }
+  
+  function prependCat (name) {
+    return [name, ...cats];
+  }
+  
+  function removeFirstCat () {
+    return cats.slice(1);
+  }
+  
+  function removeLastCat () {
+    return cats.slice(0, cats.length - 1);
+  }
